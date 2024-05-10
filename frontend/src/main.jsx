efdfb7d9da0aeba0582deeeb8a9570ecc45d385e
luigi059 <luigimarkfernandez@gmail.com>
2024-05-10 18:04:19 +0100

    Admin Routes WIP FE

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,6 +6,8 @@ import { createBrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import PrivateRoute from './components/PrivateRoute.jsx';
 import './index.css';
+import AdminRoutes from './pages/Admin/AdminRoutes.jsx';
+import UserList from './pages/Admin/UserList.jsx';
 import Login from './pages/Auth/Login.jsx';
 import Register from './pages/Auth/Register.jsx';
 import UpdateProfile from './pages/User/UpdateProfile.jsx';
@@ -19,6 +21,9 @@ const router = createBrowserRouter(
 			</Route>
 			<Route path="/login" element={<Login />} />
 			<Route path="/register" element={<Register />} />
+			<Route path="/admin" element={<AdminRoutes />}>
+				<Route path="userList" element={<UserList />} />
+			</Route>
 		</Route>
 	)
 );
