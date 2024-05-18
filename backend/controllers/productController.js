65e6fe1c0f34555ad3a0b1791ac83db9bab9fdf9
luigi059 <luigimarkfernandez@gmail.com>
2024-05-18 16:21:47 +0100

    update Product BE

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -22,4 +22,29 @@ const createProduct = asyncHandler(async (req, res) => {
 	res.status(200).json(product);
 });
 
-export { createProduct };
+const updateProduct = asyncHandler(async (req, res) => {
+	const { name, description, price, category, quantity, brand } = req.fields;
+	switch (true) {
+		case !name:
+			return res.status(400).json({ error: 'Name is required' });
+		case !brand:
+			return res.status(400).json({ error: 'Brand is required' });
+		case !description:
+			return res.status(400).json({ error: 'Description is required' });
+		case !price:
+			return res.status(400).json({ error: 'Price is required' });
+		case !category:
+			return res.status(400).json({ error: 'Category is required' });
+		case !quantity:
+			return res.status(400).json({ error: 'Quantity is required' });
+	}
+	const product = await Product.findByIdAndUpdate(
+		req.params.id,
+		{ ...req.fields },
+		{ new: true }
+	);
+	await product.save();
+	res.json(product);
+});
+
+export { createProduct, updateProduct };
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import formidable from 'express-formidable';
-import { createProduct } from '../controllers/productController.js';
+import {
+	createProduct,
+	updateProduct,
+} from '../controllers/productController.js';
 import { authenticate, authorizeAdmin } from '../middlewares/authMiddleware.js';
 import checkId from '../middlewares/checkId.js';
 
@@ -8,5 +11,8 @@ const router = express.Router();
 router
 	.route('/')
 	.post(authenticate, authorizeAdmin, formidable(), createProduct);
+router
+	.route('/:id')
+	.put(authenticate, authorizeAdmin, formidable(), updateProduct);
 
 export default router;
